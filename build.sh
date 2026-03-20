0663a1963dcc8d33c8775795b08ec142bf8c87ef
J0113 <J0113@users.noreply.github.com>
2026-03-20 11:44:22 +0100

    Improved Build script

--- build.sh
+++ build.sh
@@ -16,5 +16,10 @@ echo
 dotnet publish "$PROJECT" -r win-x64   $FLAGS -o publish/win-x64
 echo
 
+mkdir -p publish/release
+mv publish/osx-arm64/SCAScanner  publish/release/SCAScanner-osx-arm64
+mv publish/linux-x64/SCAScanner  publish/release/SCAScanner-linux-x64
+mv publish/win-x64/SCAScanner.exe publish/release/SCAScanner-win-x64.exe
+
 echo "Build complete:"
-ls -lh publish/osx-arm64/SCAScanner publish/linux-x64/SCAScanner publish/win-x64/SCAScanner.exe
+ls -lh publish/release/SCAScanner-osx-arm64 publish/release/SCAScanner-linux-x64 publish/release/SCAScanner-win-x64.exe
